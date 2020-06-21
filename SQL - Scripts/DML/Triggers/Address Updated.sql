98cc4eb2d6707050226f639a059920794b906a71
KennethS0 <kenneth.sanchez0609@gmail.com>
2020-06-21 17:06:04 -0600

    Audit Log Triggers

--- SQL - Scripts/DML/Triggers/Address Updated.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER address_updated
-BEFORE UPDATE
-ON address
-FOR EACH ROW
-BEGIN
-    :new.modification_user := USER;
-    :new.modification_date := SYSDATE;
-END;
-/
--- SQL - Scripts/DML/Triggers/Admin Bans Updated.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE TRIGGER adminbans_updated
-BEFORE UPDATE
-ON ADMINBANS
-FOR EACH ROW
-BEGIN
-    :new.modification_user := USER;
-    :new.modification_date := SYSDATE;
-END;
-/
