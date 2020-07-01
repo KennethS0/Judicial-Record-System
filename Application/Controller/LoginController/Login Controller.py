e33efda51e6e6e769b2714e3d7413c69bb6bdc13
KennethS0 <kenneth.sanchez0609@gmail.com>
2020-07-01 02:21:13 -0600

    Refactoring

---
+++ Application/Controller/LoginController/Login Controller.py
@@ -0,0 +1,10 @@
+import Application.Model.DatabaseConnection.Database as dbo
+
+class LogInController:
+
+    def __init__(self, pDatabase, pView):
+        self.database = pDatabase
+        self.view = pView
+
+if __name__ == '__main__':
+    print('lmao')
